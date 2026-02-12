ff5c2c2061076bb2953f6bdaddf727faac1faf41
TomDixon97 <dixontom97@gmail.com>
2026-02-12 16:22:17 +0000

    added add_track function and tests

--- lib/music_list.py
+++ lib/music_list.py
@@ -1,3 +1,8 @@
 class MusicHistory():
     def __init__(self):
         self.track_list = []
+
+    def add_track(self, song):
+        if not isinstance(song, str):
+            raise TypeError("Song must be a string")
+        self.track_list.append(song)
--- tests/test_music_list.py
+++ tests/test_music_list.py
@@ -1,5 +1,5 @@
 from lib.music_list import MusicHistory
-
+import pytest
 """
 When class initialised, check empty list is created
 """
@@ -8,3 +8,21 @@ def test_list_is_initialised():
     expected = []
 
     assert history.track_list == expected
+
+def test_add_track_as_string():
+    history = MusicHistory()
+    history.add_track('song1')
+    history.add_track('song2')
+    history.add_track('song3')
+
+    expected = ['song1', 'song2', 'song3']
+    assert history.track_list == expected
+
+def test_add_track_isnt_string():
+    history = MusicHistory()
+
+    with pytest.raises(TypeError) as error:
+        history.add_track(5)
+    error_message = str(error.value)
+    assert error_message == "Song must be a string"
+
